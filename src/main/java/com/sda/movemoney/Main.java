62483255a469e1a3419bd9a844c3e93e98548c66
wielki.borsuk <wielki.borsuk@gmail.com>
2019-07-28 22:06:13 +0200

    add logging setup

--- src/main/java/com/sda/movemoney/Main.java
+++ src/main/java/com/sda/movemoney/Main.java
@@ -1,10 +1,15 @@
 package com.sda.movemoney;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        LOGGER.info("zaczynamy transakcje");
         Osoba osoba1 = new Osoba("Franciszek");
         Osoba osoba2 = new Osoba("Zenon");
 
